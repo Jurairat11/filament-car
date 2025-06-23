f32588251495709e4ab8c71963c79b45f00fac43
Jurairat11 <141710195+Jurairat11@users.noreply.github.com>
2025-06-23 08:45:32 +0700

    remove unnecessary blank line in Car_report model

--- app/Models/Car_report.php
+++ app/Models/Car_report.php
@@ -9,7 +9,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Car_report extends Model
 {
     use HasFactory;
-
     protected $primaryKey = 'id';
     protected $fillable = [
         'car_no',
