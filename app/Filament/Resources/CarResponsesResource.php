d2c456dd25d81077b2e6f1cdf04198ddbb85b598
Jurairat11 <141710195+Jurairat11@users.noreply.github.com>
2025-06-26 12:01:32 +0700

    fix: update 'Permanent action' tab to enable based on temporary action input

--- app/Filament/Resources/CarResponsesResource.php
+++ app/Filament/Resources/CarResponsesResource.php
@@ -133,7 +133,17 @@ class CarResponsesResource extends Resource
 
                         ]),
                     Tabs\Tab::make('Permanent action')
-                        ->disabled()
+                        ->tabs(function ($get){
+                            if($get ('temp_decs') !== null) {
+                                return [
+
+                                ];
+                            }
+                            return [
+                            /* other tabs */
+                                ];
+                            })
+
                         ->schema([
                             Textarea::make('perm_desc')
                             ->label('Permanent action')
