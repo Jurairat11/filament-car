54c147800660207c3fa08caf34a71435378fb470
Jurairat11 <141710195+Jurairat11@users.noreply.github.com>
2025-07-01 10:50:31 +0700

    fix: clean up ViewCarReport by removing unused imports and commented-out code

--- app/Filament/Pages/ViewCarReport.php
+++ app/Filament/Pages/ViewCarReport.php
@@ -2,34 +2,25 @@
 
 namespace App\Filament\Pages;
 
-use App\Models\Problem;
 use Filament\Pages\Page;
-use Filament\Infolists\Infolist;
 use Illuminate\Support\Facades\Auth;
-use Filament\Forms\Components\Placeholder;
-use Filament\Infolists\Components\Section;
-use Filament\Infolists\Components\TextEntry;
+
+use Filament\Resources\Pages\Concerns\InteractsWithRecord;
 
 class ViewCarReport extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
     protected static string $view = 'filament.pages.view-car-report';
-    public Problem $problem;
 
-    public static function infolist(Infolist $infolist, Problem $problem)
+    use InteractsWithRecord;
+
+    public function mount(int | string $record): void
     {
-        // Define your infolist fields here
-        return $infolist
-        ->record($problem)
-        ->schema([
-            Section::make('Problem Information')
-                ->schema([
-                    TextEntry::make('prob_id'),
+        $this->record = $this->problem($record);
+    }
 
-                ])
-                ->collapsed()
-                ->columns(5),
-        ]);
+
+
 }
 
     // public static function shouldRegisterNavigation(): bool
@@ -37,5 +28,3 @@ class ViewCarReport extends Page
     //     return Auth::check() && Auth::user()?->role === 'Safety'; // ซ่อนเมนูจาก sidebar
     // }
 
-
-}
