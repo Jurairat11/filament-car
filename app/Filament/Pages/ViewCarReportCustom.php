b7756356aa07a0b33df0dc13d9b908d77c38a33c
Jurairat11 <141710195+Jurairat11@users.noreply.github.com>
2025-07-03 10:03:54 +0700

    fix: enhance CAR Information section with additional placeholders and view components in ViewCarReportCustom page

--- app/Filament/Pages/ViewCarReportCustom.php
+++ app/Filament/Pages/ViewCarReportCustom.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use Filament\Forms\Form;
 use Filament\Pages\Page;
 use App\Models\Car_report;
+use Filament\Forms\Components\View;
 use Illuminate\Support\Facades\Auth;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Contracts\HasForms;
@@ -37,13 +38,21 @@ class ViewCarReportCustom extends Page implements HasForms
     public function form( Form $form): Form {
         return $form->schema([
             Section::make('CAR Information')
-            ->description(fn ($livewire) =>
-                    'CAR No: ' . ($livewire->form->getRawState()['car_no'] ?? '')
-                )
+            // ->description(fn ($livewire) =>
+            //         'CAR No: ' . ($livewire->form->getRawState()['car_no'] ?? '')
+            //     )
             ->schema([
-                // Placeholder::make('car_no')
-                //     ->label('Car No.')
-                //     ->content(fn () => $this->car_report->car_no ?? '-'),
+                Placeholder::make('car_no')
+                    ->label('Car No.')
+                    ->content(fn () => $this->car_report->car_no ?? '-'),
+
+                Placeholder::make('dept_id')
+                    ->label('Department')
+                    ->content(fn () => optional ($this->car_report->department)->dept_name ),
+
+                Placeholder::make('sec_id')
+                    ->label('Section')
+                    ->content(fn()=>optional($this->car_report->section)->sec_name),
 
                 Placeholder::make('car_date')
                     ->label('Create Date')
@@ -53,11 +62,47 @@ class ViewCarReportCustom extends Page implements HasForms
                     ->label('Due date')
                     ->content(fn()=>Carbon::parse($this->car_report->car_due_date)->format('d/m/Y') ?? '-'),
 
-                Placeholder::make('d')
+                Placeholder::make('hazard_source_id')
+                        ->label('Hazard source')
+                        ->content(fn () => optional ($this->car_report->hazardSource)->source_name),
 
-                // Placeholder::make('status')
-                //     ->label('Status')
-                //     ->content(fn () => $this->car_report->status ?? '-'),
+                    Placeholder::make('place_id')
+                        ->label('Place')
+                        ->content(fn () => optional ($this->car_report->Place)->place_name),
+
+                    Placeholder::make('equipment')
+                        ->label('Equipment')
+                        ->content(fn()=>$this->car_report->equipment),
+
+                    Placeholder::make('responsible_dept_id')
+                        ->label('Reported to')
+                        ->content(fn () => optional ($this->car_report->responsible)->dept_name ),
+
+                    Placeholder::make('car_desc')
+                        ->label('Description')
+                        ->columnSpan(2)
+                        ->content(fn()=>$this->car_report->car_desc),
+
+                    Placeholder::make('hazard_level_id')
+                        ->label('Hazard level')
+                        ->content(fn () => optional ($this->car_report->hazardLevel)->level_name ),
+
+                    Placeholder::make('hazard_type_id')
+                        ->label('Hazard type')
+                        ->content(fn () => optional ($this->car_report->hazardType)->type_name ),
+
+                    View::make('components.car-reports-view-image')
+                        ->label('Before Image')
+                        ->viewData([
+                            'path' => $this->getRecord()->img_before_path,
+                        ])->columnSpan(2),
+
+                    Placeholder::make('reopen_car_reason')
+                        ->label('Cause for reopening CAR')
+                        ->content(fn () => $this->car_report->reopen_car_reason ?? '-')
+                        ->visible(fn () => $this->car_report->status === 'reopened')
+                        ->columns(2),
+
 
             ])->collapsed(),
 
