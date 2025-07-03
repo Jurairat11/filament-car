4763440a32ff2fdb7bfd245953959566acac9504
Jurairat11 <141710195+Jurairat11@users.noreply.github.com>
2025-07-03 08:41:58 +0700

    feat: implement form rendering in ViewCarReportCustom page

--- app/Filament/Pages/ViewCarReportCustom.php
+++ app/Filament/Pages/ViewCarReportCustom.php
@@ -2,34 +2,39 @@
 
 namespace App\Filament\Pages;
 
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
 use Filament\Pages\Page;
 use App\Models\Car_report;
 use Illuminate\Support\Facades\Auth;
 
-class ViewCarReportCustom extends Page
+class ViewCarReportCustom extends Page implements HasForms
 {
+
+    use InteractsWithForms;
+    public ?array $data = [];
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
     protected static string $view = 'filament.pages.view-car-report-custom';
-    public ?Car_report $record = null;
+    //public ?Car_report $record = null;
 
     // public function mount($record): void
     // {
     //     $this->record = Car_report::findOrFail($record);
     // }
 
-    public function mount(int | string $record): void
-    {
-        $this->record = $this->Car_report($record);
-    }
+    // public function mount(int | string $record): void
+    // {
+    //     $this->record = $this->Car_report($record);
+    // }
 
     public function getTitle(): string
     {
         return 'View Car Report';
     }
-    public static function shouldRegisterNavigation(): bool
-    {
-        return Auth::check() && Auth::user()?->role === 'Safety'; // ซ่อนเมนูจาก sidebar
-    }
+    // public static function shouldRegisterNavigation(): bool
+    // {
+    //     return Auth::check() && Auth::user()?->role === 'Safety'; // ซ่อนเมนูจาก sidebar
+    // }
 }
 
 
