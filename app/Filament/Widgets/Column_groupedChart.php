b9e87a3674dc1a40cae6202fab9e0b94a45fca1d
Jurairat11 <141710195+Jurairat11@users.noreply.github.com>
2025-07-16 11:30:33 +0700

    change to gray color

--- app/Filament/Widgets/Column_groupedChart.php
+++ app/Filament/Widgets/Column_groupedChart.php
@@ -216,7 +216,7 @@ class Column_groupedChart extends ApexChartWidget
             'dataLabels' => [
                 'enabled' => true,
                 'style' => [
-                    'colors' => ['#f5f5f5']
+                    'colors' => ['#c0c0c0']
                 ]
             ],
             'colors' => [ '#3b82f6','#10b981','#f59e0b','#ef4444']
