1a75ccdcfbf63089a5643ba69a645da8d3cb05f0
Jurairat11 <141710195+Jurairat11@users.noreply.github.com>
2025-07-16 11:28:57 +0700

    change datalabels color

--- app/Filament/Widgets/Column_groupedChart.php
+++ app/Filament/Widgets/Column_groupedChart.php
@@ -216,7 +216,7 @@ class Column_groupedChart extends ApexChartWidget
             'dataLabels' => [
                 'enabled' => true,
                 'style' => [
-                    'colors' => ['#808080']
+                    'colors' => ['#f5f5f5']
                 ]
             ],
             'colors' => [ '#3b82f6','#10b981','#f59e0b','#ef4444']
