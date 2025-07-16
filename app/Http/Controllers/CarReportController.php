1c3ffbf26d66d053a5895d33c95e590d473604b7
Jurairat11 <141710195+Jurairat11@users.noreply.github.com>
2025-07-16 11:48:54 +0700

    car controller

--- app/Http/Controllers/CarReportController.php
+++ app/Http/Controllers/CarReportController.php
@@ -8,24 +8,24 @@ use Illuminate\Support\Facades\DB;
 
 class CarReportController extends Controller
 {
-    public function CreateCarReport(Request $request)
-    {
-        try {
-            $report = DB::transaction(function () use ($request) {
-                // นับแบบ lock เพื่อไม่ให้ซ้ำ
-                $count = Car_report::whereYear('created_at', now()->year)->lockForUpdate()->count() + 1;
-                $carNo = 'C-' . now()->format('y') . '_' . str_pad($count, 3, '0', STR_PAD_LEFT);
+    // public function CreateCarReport(Request $request)
+    // {
+    //     try {
+    //         $report = DB::transaction(function () use ($request) {
+    //             // นับแบบ lock เพื่อไม่ให้ซ้ำ
+    //             $count = Car_report::whereYear('created_at', now()->year)->lockForUpdate()->count() + 1;
+    //             $carNo = 'C-' . now()->format('y') . '_' . str_pad($count, 3, '0', STR_PAD_LEFT);
 
-                $data = $request->all();
-                $data['car_no'] = $carNo;
+    //             $data = $request->all();
+    //             $data['car_no'] = $carNo;
 
-                return Car_report::create($data);
-            });
+    //             return Car_report::create($data);
+    //         });
 
-            return response()->json($report, 201);
+    //         return response()->json($report, 201);
 
-        } catch (\Exception $e) {
-            return response()->json(['error' => 'สร้างไม่สำเร็จ: ' . $e->getMessage()], 500);
-        }
-    }
+    //     } catch (\Exception $e) {
+    //         return response()->json(['error' => 'สร้างไม่สำเร็จ: ' . $e->getMessage()], 500);
+    //     }
+    // }
 }
