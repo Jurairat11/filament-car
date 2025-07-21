069fb08f4149f44b9cb5d664dc37b66c6c26dd80
Jurairat11 <141710195+Jurairat11@users.noreply.github.com>
2025-07-21 13:15:01 +0700

    add notification in delay status command

--- app/Console/Commands/DelayStatus.php
+++ app/Console/Commands/DelayStatus.php
@@ -3,8 +3,12 @@
 namespace App\Console\Commands;
 
 use Carbon\Carbon;
+use App\Models\User;
+use App\Models\Car_report;
+use Illuminate\Support\Str;
 use App\Models\Car_responses;
 use Illuminate\Console\Command;
+use Filament\Notifications\Notification;
 
 class DelayStatus extends Command
 {
@@ -41,6 +45,61 @@ class DelayStatus extends Command
                 $item->save();
             }
         }
+
+        $item = $items->first(); // Get the first item to use for notification
+
+            $report = Car_report::find($item->car_id);
+
+            if ($report) {
+                    // Notify users in dept_id
+                    if ($report->dept_id) {
+                        $users = User::where('dept_id', $report->dept_id)->get();
+                        foreach ($users as $user) {
+                            Notification::make()
+                                ->title("CAR Overdue")
+                                ->icon('heroicon-o-exclamation-triangle')
+                                ->iconColor('danger')
+                                ->body("CAR no.{$report->car_no} has no replied")
+                                ->success()
+                                ->sendToDatabase($user);
+                        }
+                    }
+
+                    // Notify users in responsible_dept_id
+                    if ($report->responsible_dept_id) {
+                        $users = User::where('dept_id', $report->responsible_dept_id)->get();
+                        foreach ($users as $user) {
+                            Notification::make()
+                                ->title("CAR Overdue")
+                                ->icon('heroicon-o-exclamation-triangle')
+                                ->iconColor('danger')
+                                ->body("CAR no.{$report->car_no} has overdue")
+                                ->success()
+                                ->sendToDatabase($user);
+                        }
+            }
+        }
+            $data = [
+                        'car_id' => $item->carReport->car_no ?? '-',
+                        'cause' => $item->cause ?? '-',
+                        'created_by' => $item->createdResponse->emp_id?? '-',
+                    ];
+
+                    $txtTitle = "ใบ CAR เลยกำหนดการตอบกลับ";
+
+                    // create connector instance
+                    $connector = new \Sebbmyr\Teams\TeamsConnector(env('MSTEAM_API'));
+                    // // create card
+                    // $card  = new \Sebbmyr\Teams\Cards\SimpleCard(['title' => $data['title'], 'text' => $data['description']]);
+
+                    // create a custom card
+                    $card  = new \Sebbmyr\Teams\Cards\CustomCard("พนักงาน " . Str::upper($data['created_by']), "หัวข้อ: " . $txtTitle);
+                    // add information
+                    $card->setColor('01BC36')
+                        ->addFacts('รายละเอียด', ['เลขที่ CAR ' => $data['car_id'], 'ความไม่ปลอดภัย' => $data['cause']])
+                        ->addAction('Visit Issue', route('filament.admin.resources.car-responses.view', $item));
+                    // send card via connector
+                    $connector->send($card);
 
 
     }
